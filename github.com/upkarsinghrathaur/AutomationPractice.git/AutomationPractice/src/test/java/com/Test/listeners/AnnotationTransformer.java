392a9a4be0abfc8950997b75f91d520aa17ce384
Upkar Singh <upkarsinghj@gmail.com>
2020-07-20 20:08:15 +0530

    Added extent reporting

--- github.com/upkarsinghrathaur/AutomationPractice.git/AutomationPractice/src/test/java/com/Test/listeners/AnnotationTransformer.java
+++ github.com/upkarsinghrathaur/AutomationPractice.git/AutomationPractice/src/test/java/com/Test/listeners/AnnotationTransformer.java
@@ -1,9 +1,11 @@
-package com.Test.utilities;
+package com.Test.listeners;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 
 import org.testng.IAnnotationTransformer;
 import org.testng.annotations.ITestAnnotation;
+
+import com.Test.utilities.RetryAnalyzer;
 
 public class AnnotationTransformer implements IAnnotationTransformer{
 
---
+++ github.com/upkarsinghrathaur/AutomationPractice.git/AutomationPractice/src/test/java/com/Test/listeners/ExtentReportListeners.java
@@ -0,0 +1,72 @@
+package com.Test.listeners;
+import java.util.Arrays;
+import java.util.Date;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.markuputils.ExtentColor;
+import com.aventstack.extentreports.markuputils.Markup;
+import com.aventstack.extentreports.markuputils.MarkupHelper;
+
+public class ExtentReportListeners implements ITestListener {
+
+	Date d = new Date();
+	String fileName = "Extent_" + d.toString().replace(":", "_").replace(" ", "_") + ".html";
+	ExtentReports extent = ExtentManager.createInstance(System.getProperty("user.dir")+"\\reports\\"+fileName);
+	public static ThreadLocal<ExtentTest> testReport = new ThreadLocal<ExtentTest>();
+	
+	public void onFinish(ITestContext arg0) {
+		// TODO Auto-generated method stub
+		if (extent != null)
+		{
+			extent.flush();
+		}
+	}
+
+	public void onStart(ITestContext arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestFailure(ITestResult result) {
+		// TODO Auto-generated method stub
+		String excepionMessage=Arrays.toString(result.getThrowable().getStackTrace());
+		testReport.get().fail("<details>" + "<summary>" + "<b>" + "<font color=" + "red>" + "Exception Occured: Click Here"
+				+ "</font>" + "</b >" + "</summary>" +excepionMessage.replaceAll(",", "<br>")+"</details>"+" \n");				
+		String methodName=result.getMethod().getMethodName();
+		String failureLogg="<b>"+"TEST CASE:- "+ methodName.toUpperCase()+ "  FAILED"+"</b>";
+		Markup m = MarkupHelper.createLabel(failureLogg, ExtentColor.RED);
+		testReport.get().log(Status.FAIL, m);
+	}
+
+	public void onTestSkipped(ITestResult result) {
+		// TODO Auto-generated method stub
+		String methodName=result.getMethod().getMethodName();
+		String logText="<b>"+"Test Case:- "+ methodName+ " Skipped"+"</b>";		
+		Markup m=MarkupHelper.createLabel(logText, ExtentColor.YELLOW);
+		testReport.get().log(Status.SKIP, m);
+	}
+
+	public void onTestStart(ITestResult result) {
+		// TODO Auto-generated method stub
+		ExtentTest test = extent.createTest("     @TestCase : For "+ result.getTestClass().getRealClass().getSimpleName()+" - "+result.getMethod().getMethodName());
+        testReport.set(test);
+	}
+
+	public void onTestSuccess(ITestResult result) {
+		// TODO Auto-generated method stub
+		String methodName=result.getMethod().getMethodName();
+		String logText="<b>"+"TEST CASE:- "+ methodName.toUpperCase()+ " PASSED"+"</b>";		
+		Markup m=MarkupHelper.createLabel(logText, ExtentColor.GREEN);
+		testReport.get().pass(m);
+	}
+
+}
